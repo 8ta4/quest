0bdca63670a1a79b64d3269720f5290c0304b99e
a <a@as-Virtual-Machine.local>
2024-09-04 23:10:33 -0700

    Configure pre-commit hooks in devenv.nix for code quality

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.gitleaks
+  ];
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
@@ -29,6 +32,22 @@
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    gitleaks = {
+      enable = true;
+      # https://github.com/gitleaks/gitleaks/blob/39947b0b0d3f1829438000819c1ba9dbeb023a89/.pre-commit-hooks.yaml#L4
+      entry = "gitleaks protect --verbose --redact --staged";
+    };
+    nixpkgs-fmt.enable = true;
+    prettier.enable = true;
+    # https://github.com/cachix/git-hooks.nix/issues/31#issuecomment-744657870
+    trailing-whitespace = {
+      enable = true;
+      # https://github.com/pre-commit/pre-commit-hooks/blob/4b863f127224b6d92a88ada20d28a4878bf4535d/.pre-commit-hooks.yaml#L201-L207
+      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+      types = [ "text" ];
+    };
+  };
 
   # https://devenv.sh/processes/
   # processes.ping.exec = "ping example.com";
