cec8212f4d39c542402d1352ed86547c0d942a12
a <a@as-Virtual-Machine.local>
2024-09-17 20:24:44 -0700

    Add questions function to render questions list from message data

--- src/question.cljs
+++ src/question.cljs
@@ -5,8 +5,16 @@
 (def id
   (int (:id (query-map js/location.href))))
 
+(defn questions
+  [{:keys [qa]}]
+  [:ol (map (comp (partial vector :li)
+                  :question)
+            qa)])
+
 (defn init
   []
   (js/console.log "Question script initialized")
-  (.onMessage.addListener (js/chrome.tabs.connect id) (fn [message]
-                                                        (js->clj message {:keywordize-keys true}))))
+  (.onMessage.addListener (js/chrome.tabs.connect id)
+                          (fn [message]
+                            (rdom/render [questions (js->clj message {:keywordize-keys true})]
+                                         (js/document.getElementById "app")))))
