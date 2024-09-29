e3689b66f103ad9457abba9344513797cf275cfb
a <a@as-Virtual-Machine.local>
2024-09-29 01:04:22 -0700

    Simplify background script by removing unnecessary functions

--- src/background.cljs
+++ src/background.cljs
@@ -2,19 +2,6 @@
   (:require [shadow.cljs.modern :refer [js-await]]
             [lambdaisland.fetch :as fetch]))
 
-(when js/goog.DEBUG
-  (defn remove-popup-windows []
-    (js-await [windows (js/chrome.windows.getAll)]
-              (->> (js->clj windows {:keywordize-keys true})
-                   (filter (comp (partial = "popup")
-                                 :type))
-                   (map :id)
-                   (run! js/chrome.windows.remove)))))
-
-(defn create-question-window [id]
-  (js/chrome.windows.create (clj->js {:url (str "localhost:8000/question.html?id=" id)
-                                      :type "popup"})))
-
 (defn init
   []
   (js/console.log "Background script initialized")
