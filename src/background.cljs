1212203dfc406c2720c33551b8f938d9d1c12dae
a <a@as-Virtual-Machine.local>
2024-09-29 20:35:09 -0700

    Simplify init function in background.cljs

--- src/background.cljs
+++ src/background.cljs
@@ -1,13 +1,5 @@
-(ns background
-  (:require [shadow.cljs.modern :refer [js-await]]
-            [lambdaisland.fetch :as fetch]))
+(ns background)
 
 (defn init
   []
-  (js/console.log "Background script initialized")
-  (js/chrome.runtime.onConnect.addListener
-   (fn [port]
-     (js/console.log "Connected to content script")
-     (js/console.log port.name)
-     (js-await [response (fetch/get port.name)]
-               (.postMessage port (:body response))))))
+  (js/console.log "Background script initialized"))
--- src/content.cljs
+++ src/content.cljs
@@ -1,9 +1,7 @@
 (ns content
   (:require [clojure.string :as str]
-            [com.rpl.specter :refer [ALL ATOM END nthpath setval transform]]
-            [lambdaisland.uri :refer [query-map]]
-            [shadow.cljs.modern :refer [js-await]]
-            [yaml :refer [parse]]))
+            [com.rpl.specter :refer [ATOM END nthpath setval transform]]
+            [lambdaisland.uri :refer [query-map]]))
 
 (defonce state
   (atom {}))
@@ -183,17 +181,4 @@
   (set! js/document.onkeydown handle))
 
 (defn init
-  []
-  (when quest
-    (let [port (js/chrome.runtime.connect (clj->js {:name quest}))]
-      (js/console.log "Connected to background script")
-      (port.onMessage.addListener
-       (fn [response]
-         (js/console.log "Received response from background script")
-         (reset! state {:qa (setval [ALL :visible]
-                                    false
-                                    (js->clj (parse response) {:keywordize-keys true}))
-                        :id 0})
-         (when js/goog.DEBUG
-           (reset! body (js/document.body.cloneNode true)))
-         (after-load))))))
+  [])
