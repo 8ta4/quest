bd629b83cf1b9bbdbaeb66c4781c2dace640c05d
a <a@as-Virtual-Machine.local>
2024-09-29 20:48:37 -0700

    Enhance onCommitted listener to set URL in state atom

--- src/background.cljs
+++ src/background.cljs
@@ -1,6 +1,17 @@
-(ns background)
+(ns background
+  (:require [com.rpl.specter :refer [ATOM setval]]))
+
+(def state (atom {}))
+
+(defn eval-path-setval
+  "Sets the value `aval` at the specified path `apath` within the `structure`.
+   The path is dynamically evaluated, allowing for runtime determination of where
+   the value should be set."
+  [apath aval structure]
+  (setval apath aval structure))
 
 (defn init
   []
   (js/console.log "Background script initialized")
-  (js/chrome.webNavigation.onCommitted.addListener (fn [details])))
+  (js/chrome.webNavigation.onCommitted.addListener (fn [details]
+                                                     (eval-path-setval [ATOM details.tabId] details.url state))))
