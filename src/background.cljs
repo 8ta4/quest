5f0d0b6ab8c274ac3188fe8dd4daddfa0c22041c
a <a@as-Virtual-Machine.local>
2024-10-01 21:11:20 -0700

    Update manifest.json to use production URLs for question.html

--- src/background.cljs
+++ src/background.cljs
@@ -14,7 +14,7 @@
   (setval apath aval structure))
 
 (defn create-question-window [port]
-  (js/chrome.windows.create (clj->js {:url "http://localhost:8000/question.html"
+  (js/chrome.windows.create (clj->js {:url "https://8ta4.github.io/quest/public/question.html"
                                       :type "popup"})
                             (fn [window]
                               (eval-path-setval [ATOM
